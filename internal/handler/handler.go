512693ff62354f8f623174042757f7ac134f23ac
pipobukapi <savotiya@gmail.com>
2026-04-29 11:15:46 +0300

    removed some logs from handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,9 +3,8 @@ package handler
 import (
 	"encoding/json"
 	"errors"
-	"io"
-	"log"
 	"net/http"
+	""
 
 	"github.com/sastromikus/pip_diploma_gofermarket/internal/model"
 	"github.com/sastromikus/pip_diploma_gofermarket/internal/service"
@@ -26,25 +25,15 @@ func NewHandler(auth AuthService) *Handler {
 }
 
 func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Printf("read body error: %v", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	log.Printf("raw body: %q", string(body))
-
 	var req model.AuthRequest
-	if err := json.Unmarshal(body, &req); err != nil {
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		log.Printf("decode error: %v", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	log.Printf("register request: login=%q password=%q", req.Login, req.Password)
-
-	_, err = h.auth.Register(req.Login, req.Password)
+	_, err := h.auth.Register(req.Login, req.Password)
 	if err != nil {
 		log.Printf("register error: %v", err)
 
